efa37779e5d8d96b934d0953ad3b04945a3c51bc
robynchung <jaljung@hanmail.net>
2018-04-18 15:00:18 -0400

    added javadocs

--- Test/src/teamproject/Question.java
+++ Test/src/teamproject/Question.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 
 /**
  *
- * @author c0692516
+ * @author c0692516 Michael Patoine
  */
 public class Question implements Serializable
 {
@@ -19,10 +19,23 @@ public class Question implements Serializable
     private String question;
     private ArrayList<Answer> answers;
 
+    /**
+     * method for getting id
+     * @return
+     */
     public int getId() {
         return id;
     }
     
+    /**
+     * Class constructor
+     * @param id
+     * @param quest
+     * @param q1
+     * @param q2
+     * @param q3
+     * @param q4
+     */
     public Question(int id, String quest, String q1, String q2, String q3, String q4)
     {
         this.id = id;
@@ -34,15 +47,36 @@ public class Question implements Serializable
         answers.add(new Answer(q4));
     }
     
+    /**
+     * method for getting question
+     * @return
+     */
     public String GetQuestion(){return question;}
+
+    /**
+     * method for setting question
+     * @param s
+     */
     public void SetQuestion(String s){question = s;}
+
+    /**
+     * method for getting answers array list
+     * @return answers
+     */
     public ArrayList<Answer> GetAnswers(){return answers;}
     
+    /**
+     * method for shuffling answers
+     */
     public void ShuffleAnswers()
     {
         Collections.shuffle(answers);
     }
     
+    /**
+     * method for getting correct answers
+     * @return
+     */
     public int GetCorrectAnswer()
     {
         for(Answer a : answers)
