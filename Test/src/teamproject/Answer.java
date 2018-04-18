67462291163d763e3411c30f5aeacba817213005
robynchung <jaljung@hanmail.net>
2018-04-18 14:13:57 -0400

    java doc added

--- Test/src/teamproject/Answer.java
+++ Test/src/teamproject/Answer.java
@@ -8,27 +8,55 @@ package teamproject;
 import java.io.Serializable;
 
 /**
- * @author c0692516
+ * @author c0692516 Michael Patoine
  */
 public class Answer implements Serializable
 {
     private String sAnswer;
     private boolean bCorrect;
     
+    /**
+     * Class constructor
+     * @param answer
+     */
     public Answer(String answer)
     {
         sAnswer = answer;
         bCorrect = false;
     }
     
+    /**
+     * Class constructor
+     * @param answer
+     * @param correct
+     */
     public Answer(String answer, boolean correct)
     {
         sAnswer = answer;
         bCorrect = correct;
     }
     
+    /**
+     * getting answer
+     * @return
+     */
     public String GetAnswer(){return sAnswer;}
+
+    /**
+     * setting answer
+     * @param s
+     */
     public void SetAnswer(String s){sAnswer = s;}
+
+    /**
+     * getting correct answer
+     * @return
+     */
     public boolean GetCorrect(){return bCorrect;}
+
+    /**
+     * setting correct answer
+     * @param b
+     */
     public void SetCorrect(boolean b){bCorrect = b;}
 }
